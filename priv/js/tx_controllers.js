6bccee7d613824c493effa2c1a5e7022a1d85d6d
Dmytro Lytovchenko <kvakvs@yandex.ru>
2014-09-21 20:39:29 +0200

    angularize tree generation, was: static html generated recursively, now: angular nested directives

--- priv/js/tx_controllers.js
+++ priv/js/tx_controllers.js
@@ -22,93 +22,15 @@ txApp.controller('TxListCtrl', function ($scope, $http) {
 txApp.controller('TxShowCtrl', function ($scope, $http) {
   $scope.show_id = window.location.hash.substring(1);
   $http.get('/tx/tx_esi:show?' + $scope.show_id).success(function(data) {
-    $scope.term = Array([data]);
+    $scope.term_stack = [data];
+    $scope.get_term = function() {
+      return $scope.term_stack[0];
+    }
   });
   $scope.show_next_term = function(pickle) {
     console.log(pickle);
   }
 });
-
-txApp.directive('term', function ($compile) {
-  return {
-    restrict: "E",
-    replace: true,
-    scope: { term: '=' },
-    template: '<div></div>',
-    link: function (scope, element, attrs) {
-      scope.$watch(attrs.term, function (x) {
-        if (x) {
-//          scope.$apply(function() {
-//            var content = $compile(html_term(scope.term))(scope);
-//            element.append(content);
-//          });
-          var compiled_templ = $compile(html_term(x));
-          element.append(compiled_templ(scope));
-        }
-      });
-    }
-  }
-});
-
-function html_term(term) {
-  var result = '';
-  if (term.t == 't') {
-    result += '<div class="tuplebox">{ ' + collapse_container();
-    term.v.forEach(function (subterm) {
-      result += '<div class="indent">' + html_term(subterm) + '</div>';
-    });
-    result += '}</div>';
-  } else if (term.t == 'l') {
-    result += '<div class="listbox">[ ' + collapse_container();
-    term.v.forEach(function(subterm) {
-      result += '<div class="indent">' + html_term(subterm) + '</div>';
-    });
-    result += ']</div>';
-  } else if (term.t == 's') {
-    if (term.v.length < 1024) {
-      result += '&ldquo;<span class="value string">' + htmlq(term.v) +
-          '</span>&rdquo;';
-    } else {
-      result += '<div class="value string">' + collapse_container()
-          + htmlq(term.v) + '</div>';
-    }
-  } else if (term.t == 'a') {
-    result += '&lsquo;<span class="value atom">' + term.v + '</span>&rsquo;';
-  } else if (term.t == 'b') {
-    if (term.v.length < 1024) {
-      result += '<span class="binary"><span class="binaryblue value">' + htmlq(term.v) +
-          '</span></span>';
-    } else {
-      result += '<div class="binary">' + collapse_container()
-          + htmlq(term.v) + '</div>';
-    }
-  } else if (term.t == 'bs') {
-    result += '<span class="binarystr"><span class="binaryblue value">' + htmlq(term.v) +
-        '</span></span>';
-  } else if (term.t == 'p') {
-    result += '<span class="value pid">' +
-        //'<button ng-click="show_next_term(\'' + term.pickle + '\')">' +
-        term.v +
-        //'</button>' +
-        '</span>';
-  } else if (term.t == 'r') {
-    result += '<span class="value ref">' + term.v + '</span>';
-  } else if (term.t == 'port') {
-    result += '<span class="value port">' + term.v + '</span>';
-  } else if (term.t == 'i') {
-    result += '<span class="value integer">' + term.v + '</span>';
-  } else if (term.t == 'f') {
-    result += '<span class="value float">' + term.v + '</span>';
-  } else if (term.t == 'fun') {
-    result += '<span class="fun">' +
-        '<span class="value">' + term.m + '</span>:' +
-        '<span class="value">' + term.n + '</span>/' +
-        '<span class="value">' + term.a + '</span></span>';
-  } else {
-    result += '<div class="value">' + term.v + '</div>';
-  }
-  return result;
-}
 
 function htmlq(q) {
   return q.replace(/</g, '&lt;').replace(/>/g, '&gt;')
@@ -141,3 +63,83 @@ function click_buttons_under(root, cls) {
     $(value).trigger('click');
   });
 }
+
+txApp.directive('termShow', function($compile){
+  return {
+    restrict: 'A',
+    scope: { termData: '=' },
+    link: function(scope, element, attrs){
+      scope.$watch(function() { return scope.termData; }, function() {
+        if (scope.termData) {
+          element.append(term_template(scope.termData));
+          $compile(element.contents())(scope);
+        }
+      });
+    }
+  }
+});
+
+function term_template(term) {
+  if (term.t == 'l') {
+    return '<div class="listbox">[ ' + collapse_container() +
+        '<div term-collection nested-data="termData.v"></div>]</div>';
+  } else if (term.t == 't') {
+    return '<div class="tuplebox">{ ' + collapse_container() +
+        '<div term-collection nested-data="termData.v"></div>}</div>';
+  } else if (term.t == 's') {
+    if (term.v.length < 1024) { // short strings
+      return '&ldquo;<span class="value string">' + htmlq(term.v) +
+          '</span>&rdquo;';
+    } else { // long strings with collapse button
+      return '<div class="value string">' + collapse_container() +
+          htmlq(term.v) + '</div>';
+    }
+  } else if (term.t == 'a') {
+    return '&lsquo;<span class="value atom">' + htmlq(term.v) +
+        '</span>&rsquo;';
+  } else if (term.t == 'b') {
+    if (term.v.length < 1024) {
+      return '<span class="binary"><span class="binaryblue value">' +
+          htmlq(term.v) +
+          '</span></span>';
+    } else {
+      return '<div class="binary">' + collapse_container() + htmlq(term.v) +
+          '</div>';
+    }
+  } else if (term.t == 'bs') {
+    return '<span class="binarystr"><span class="binaryblue value">' +
+        htmlq(term.v) + '</span></span>';
+  } else if (term.t == 'p') {
+    return '<span class="value pid">' +
+      //'<button ng-click="show_next_term(\'' + term.pickle + '\')">' +
+      term.v +
+      //'</button>' +
+      '</span>';
+  } else if (term.t == 'r') {
+    return '<span class="value ref">' + term.v + '</span>';
+  } else if (term.t == 'port') {
+    return '<span class="value port">' + term.v + '</span>';
+  } else if (term.t == 'i') {
+    return '<span class="value integer">' + term.v + '</span>';
+  } else if (term.t == 'f') {
+    return '<span class="value float">' + term.v + '</span>';
+  } else if (term.t == 'fun') {
+    return '<span class="fun">' +
+        '<span class="value">' + term.m + '</span>:' +
+        '<span class="value">' + term.n + '</span>/' +
+        '<span class="value">' + term.a + '</span></span>';
+  }
+  return '<div>{{termData}}</div>';
+}
+
+txApp.directive('termCollection', function(){
+  return {
+    restrict: 'A',
+    replace: true,
+    scope: {
+      nestedData: '='
+    },
+    template: '<div class="indent" ng-repeat="subterm in nestedData">' +
+        '<div term-show  term-data="subterm"></div></div>'
+  };
+});
