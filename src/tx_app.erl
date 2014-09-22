763fc09090467944d0b01266c2e47b610c9b22b7
Dmytro Lytovchenko <dmytro.lytovchenko@klarna.com>
2014-09-22 14:08:50 +0200

    set port to 0 in app environment to have random port assigned on startup

--- src/tx_app.erl
+++ src/tx_app.erl
@@ -35,32 +35,43 @@ stop(_State) ->
 %% @doc Call this after tx application started
 start_web() ->
   inets:start(),
-  {ok, TxPort}   = application:get_env(tx, port),
+  {ok, TxPort} = application:get_env(tx, port),
   {ok, ConfHost} = application:get_env(tx, host),
-  {ok, TxHost}   = inet_parse:address(ConfHost),
+  {ok, TxHost} = inet_parse:address(ConfHost),
   DocRoot = priv_dir(tx),
   SrvRoot = filename:absname(DocRoot ++ "/../"),
 
-  Options    = [ {modules, [ mod_esi
-                           , mod_get ]}
+  Options = [{modules, [mod_esi
+                       , mod_get]}
 
-               , {port, TxPort}
-               , {server_name, "127.0.0.1"}
-               , {server_root, SrvRoot}
-               , {document_root, DocRoot}
-               , {bind_address, TxHost}
+            , {port, TxPort}
+            , {server_name, "127.0.0.1"}
+            , {server_root, SrvRoot}
+            , {document_root, DocRoot}
+            , {bind_address, TxHost}
 
-               , {erl_script_alias, {"/tx", [tx_esi, io]}}
+            , {erl_script_alias, {"/tx", [tx_esi, io]}}
 
-               , {mime_types, [ {"html", "text/html"}
-                              , {"css", "text/css"}
-                              , {"js", "application/x-javascript"}
-                              ]}
-               , {mime_type, "application/octet-stream"}
-               ],
-  {ok, _Pid} = inets:start(httpd, Options),
+            , {mime_types, [{"html", "text/html"}
+                           , {"css", "text/css"}
+                           , {"js", "application/x-javascript"}
+                           ]}
+            , {mime_type, "application/octet-stream"}
+            ],
+  {ok, Pid} = inets:start(httpd, Options),
+  case TxPort of
+    0 ->
+      Info = httpd:info(Pid),
+      {port, ListenPort} = proplists:lookup(port, Info),
+      application:set_env(tx, port, ListenPort);
+    _ -> ok
+  end,
+
+  %% Reread selected port (if was set to 0 in app config)
+  {ok, SelectedPort} = application:get_env(tx, port),
   io:format("~n[term explorer] http server started. Add a term using tx:show(Term) or~n", []),
-  io:format("[term explorer] visit http://~s:~p/index.html~n", [ConfHost, TxPort]).
+  io:format("[term explorer] visit http://~s:~p/index.html~n"
+           , [ConfHost, SelectedPort]).
 
 priv_dir(App) ->
   case code:priv_dir(App) of
